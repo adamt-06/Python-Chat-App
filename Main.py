a660a359e78fffc5c0bc813cb63110ccbdb6dd87
Fatal-Dev <115042556+fatal-developer@users.noreply.github.com>
2023-11-27 18:46:53 +0000

    syntax issues resolved

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import hashlib, socket, sys, re, subprocess, hmac
 
-openedFile = open(__file__,"r",encoding='utf-8');
-readFile = openedFile.read();
-progHash = hashlib.sha256(str(readFile).encode('utf-8')).hexdigest();
+openedFile = open(__file__,"r",encoding='utf-8')
+readFile = openedFile.read()
+progHash = hashlib.sha256(str(readFile).encode('utf-8')).hexdigest()
 IO = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IO.bind(("255.255.255.255", 1337))
 
@@ -14,9 +14,9 @@ print("Please Input Your Display Name For This Session\n------------------------
 displayName = re.sub('[^A-Za-z0-9]+', '', input())
 
 while(True):
-    newMsg = re.sub('[^A-Za-z0-9]+', '', input());
+    newMsg = re.sub('[^A-Za-z0-9]+', '', input())
     
-    chatLog.append(displayName + "@" + socket.getfqdn("localhost") + " : " + newMsg);
+    chatLog.append(displayName + "@" + socket.getfqdn("localhost") + " : " + newMsg)
     for i in range(len(chatLog)):
         print(chatLog[i-1])
 
